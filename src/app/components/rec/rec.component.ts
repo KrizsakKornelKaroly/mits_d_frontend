9c88fa488b688f3ff37b0cc13c102466e935e39c
OhradkaGergo <ohradkagergo@turr.hu>
2026-02-13 11:32:29 +0100

    chatbot, img generation, img recognition, logout component creation, to-do comment in main

---
+++ src/app/components/rec/rec.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rec',
+  standalone: true,
+  imports: [],
+  templateUrl: './rec.component.html',
+  styleUrl: './rec.component.scss'
+})
+export class RecComponent {
+
+}
