5dfa135c0db997c523a62464ca85f8ae6788d67e
OhradkaGergo <ohradkagergo@turr.hu>
2026-02-20 10:30:58 +0100

    added logout component, integrated into app layout, impl. gen component

--- src/app/components/gen/gen.component.ts
+++ src/app/components/gen/gen.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { FloatLabelModule } from "primeng/floatlabel"
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { Tooltip } from 'primeng/tooltip';
 
 @Component({
   selector: 'app-gen',
   standalone: true,
-  imports: [],
+  imports: [FloatLabelModule, InputTextModule, FormsModule, ButtonModule, CardModule, CommonModule, RouterModule, Tooltip],
   templateUrl: './gen.component.html',
   styleUrl: './gen.component.scss'
 })
--- src/app/components/logout/logout.component.ts
+++ src/app/components/logout/logout.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-logout',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, CommonModule, RouterModule],
   templateUrl: './logout.component.html',
   styleUrl: './logout.component.scss'
 })
