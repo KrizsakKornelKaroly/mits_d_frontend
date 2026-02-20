fcdd1e933cfcaa5b8fb42e24cde292dc4a4985d8
OhradkaGergo <ohradkagergo@turr.hu>
2026-02-20 11:18:48 +0100

    typo fixes, impl. rec component

--- src/app/components/rec/rec.component.ts
+++ src/app/components/rec/rec.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { FloatLabelModule } from "primeng/floatlabel"
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FileUpload } from 'primeng/fileupload';
 
 @Component({
   selector: 'app-rec',
   standalone: true,
-  imports: [],
+  imports: [FloatLabelModule, InputTextModule, FormsModule, ButtonModule, CardModule, CommonModule, RouterModule, FileUpload],
   templateUrl: './rec.component.html',
   styleUrl: './rec.component.scss'
 })
